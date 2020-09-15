7e16b158cc4cd58606041af311ca575fd02f1169
ShaharShi <Shaharshitrit01@gmail.com>
2020-09-15 12:50:40 +0300

    sperate mealsReducer to diff file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBarApp from "./components/ui-components/header/";
 import AppRouter from "./components/containers/appRouter";
 import { BrowserRouter as Router } from "react-router-dom";
+import mealsReducer from "components/app-reducers/meals-reducer";
 
 const initialState = {
   meals: [],
@@ -14,34 +15,6 @@ const initialState = {
 
 export const MealsContext: any = React.createContext(null);
 export const ConfigurationContext: any = React.createContext(null);
-
-interface IProps {
-  type: string; // what am i doing
-  payload: any; // what am i sending
-}
-
-function mealsReducer(state: any, action: IProps) {
-  console.log(action.type);
-  if (action.type === "ADD_MEAL") {
-    const { payload } = action;
-    return { ...state, orders: [...state.orders, payload] };
-  }
-  if (action.type === "GET_MEALS_FROM_SERVER_DONE") {
-    const { payload } = action;
-    return { ...state, meals: payload };
-  }
-  if (action.type === "REMOVE_MEAL") {
-    const { payload } = action;
-    const mealsWithoutDeletedMealArr = state.orders.filter((order: any) => { return order.name !== payload.name})
-    return { ...state, orders: [...mealsWithoutDeletedMealArr] };
-  }
-  if (action.type === "CLEAR_ORDERS") {
-    const { payload } = action;
-    return { ...state, orders: payload };
-  }
-
-  return state;
-}
 
 function App() {
   const [state, dispatch] = useReducer(mealsReducer, initialState);
---
+++ src/components/app-reducers/meals-reducer/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+interface IProps {
+    type: string; // what am i doing
+    payload: any; // what am i sending
+  }
+  
+export default function mealsReducer(state: any, action: IProps) {
+    console.log(action.type);
+    if (action.type === "ADD_MEAL") {
+      const { payload } = action;
+      return { ...state, orders: [...state.orders, payload] };
+    }
+    if (action.type === "GET_MEALS_FROM_SERVER_DONE") {
+      const { payload } = action;
+      return { ...state, meals: payload };
+    }
+    if (action.type === "REMOVE_MEAL") {
+      const { payload } = action;
+      const mealsWithoutDeletedMealArr = state.orders.filter((order: any) => { return order.name !== payload.name})
+      return { ...state, orders: [...mealsWithoutDeletedMealArr] };
+    }
+    if (action.type === "CLEAR_ORDERS") {
+      const { payload } = action;
+      return { ...state, orders: payload };
+    }
+  
+    return state;
+  }
