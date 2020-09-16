98e8f16becde9a6a3f220d01a800abf37d8027e7
ShaharShi <Shaharshitrit01@gmail.com>
2020-09-17 01:55:22 +0300

    reducer conditions converted to switch case

--- src/components/app-reducers/meals-reducer/index.tsx
+++ src/components/app-reducers/meals-reducer/index.tsx
@@ -8,41 +8,48 @@ interface IProps {
   }
   
 export default function mealsReducer(state: any, action: IProps) {
-    if (action.type === "ADD_MEAL") {
+
+  switch(action.type) {
+    case "ADD_MEAL" : {
       const { payload } = action;
       return { ...state, orders: [...state.orders, payload] };
-    }
-    if (action.type === "GET_MEALS_FROM_SERVER_DONE") {
+    } break;
+
+    case "GET_MEALS_FROM_SERVER_DONE" : {
       const { payload } = action;
       return { ...state, meals: payload };
-    }
-    if (action.type === "REMOVE_MEAL") {
+    } break;
+
+    case "REMOVE_MEAL" : {
       const { payload } = action;
       const mealsWithoutDeletedMealArr = state.orders.filter((order: any) => { return order.name !== payload.name})
       return { ...state, orders: [...mealsWithoutDeletedMealArr] };
-    }
-    if (action.type === "CLEAR_ORDERS") {
+    } break;
+
+    case "CLEAR_ORDERS" : {
       const { payload } = action;
       return { ...state, orders: payload };
-    }
-    if (action.type === "FILTER_MEAL_BY_NAME") {
+    } break;
+
+    case "FILTER_MEAL_BY_NAME" : {
       const { payload } = action;
       const mealsByName: any = state.meals.filter((meal: any) => {
         const finalValue: any = meal.name.toLowerCase()
         return finalValue.includes(payload.toLowerCase()) 
       })
-      
       return { ...state, meals: [...mealsByName]}
-    }
-    if (action.type === "FILTER_MEAL_BY_DESCRIPTION") {
+    } break;
+
+    case "FILTER_MEAL_BY_DESCRIPTION" : {
       const { payload } = action;
       const mealsByDescription: any = state.meals.filter((meal: any) => {
         const finalValue: any = meal.description.toLowerCase()
         return finalValue.includes(payload.toLowerCase()) 
       })
-
       return { ...state, meals: [...mealsByDescription]}
-    }
-  
-    return state;
+    } break;
+
+    default : return state
+    
+  }
 }
